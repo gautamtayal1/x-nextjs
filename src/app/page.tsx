839083ab964f93237e3c8bd035b1c7c09ee9fb69
Gautam Tayal <gautamtayal65@gmail.com>
2025-03-18 15:35:51 +0530

    profile page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,30 +6,29 @@ import { prisma } from "@/lib/prisma";
 
 export default async function Home() {
   
-    const posts = await prisma.post.findMany({
-      include: {
-        user: true
-      }
-    })
-    
+  const posts = await prisma.post.findMany({
+    include: {
+      user: true
+    }
+  })
   return(
     <div className="container flex">
       <Navbar />
       <div className="my-5 w-[90%] m-8 flex flex-col gap-5">
       <MainCreatePost />
       {posts.map((post) => (
-          <div key={post.id}>
-          <PostCard id={post.id}
-          content={post.content}
-          likes={post.likes}
-          createdAt={post.createdAt}
-          userId={post.userId}
-          name={post.user.name}
-          username={post.user.username}
-          profilePhoto={post.user. profilePhoto}
-          />
-          </div>
-        ))}
+        <div key={post.id}>
+        <PostCard id={post.id}
+        content={post.content}
+        likes={post.likes}
+        createdAt={post.createdAt}
+        userId={post.userId}
+        name={post.user.name}
+        username={post.user.username}
+        profilePhoto={post.user. profilePhoto}
+        />
+        </div>
+      ))}
       </div>
       <RightBar />
     </div>
